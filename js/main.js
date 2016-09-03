47dd85f0175526ba65ef3c374de46357965dccde
CaiYiLiang <877526529@qq.com>
2016-09-03 20:25:25 +0800

    game move function

--- js/main.js
+++ js/main.js
@@ -1,24 +1,34 @@
 var board = new Array(); //This is for storing 4*4 digits on the board
 var score = 0; //create an vaiable and initial the value as 0
 
+var emptyGridPos = new Array();
 var start_x = 0;
 var start_y = 0;
 var end_x = 0;
 var end_y = 0;
 
 $(document).ready(function() {
-	newGame();
+	newGame();		
+	
+	$("#new_game").click(function(){
+		newGame();		
+	});
+	
+	$(document).keydown(function(event){
+		girdMove(event);
+	})
 });
 
 function newGame() {
 	//initial the game
 	init();
 	//generate random digits on the board
-	//generateOneNumber();
-	//generateOneNumber();
+	generateOneNumber();
+	generateOneNumber();
 }
 
-function init(){	
+function init(){
+	//init the gameboard and show the background-color without digits
 	for(var x=0; x<4;x++){
 		for(var y =0;y<4;y++){
 			var gridCell = $("#grid-cell-"+x+"-"+y);
@@ -27,4 +37,95 @@ function init(){
 		}
 	}
 	
+	for(var i=0; i<4;i++){
+		board [i]=new Array();
+		for(var j =0;j<4;j++){
+			board [i][j] = 0;	
+		}
+	}
+	
+	updateBoardView();
+	
+	score=0;
+}
+
+
+function updateBoardView(){
+	 $(".number-cell").remove();
+    for( var i = 0 ; i < 4 ; i ++ )
+        for( var j = 0 ; j < 4 ; j ++ ){
+            $("#grid-container").append( '<div class="number-cell" id="number-cell-'+i+'-'+j+'"></div>' );
+            var numberCell = $("#number-cell-"+i+"-"+j);
+
+            if( board[i][j] == 0 ){
+                numberCell.css("width","0px");
+                numberCell.css("height","0px");
+                numberCell.css("top",getTop(i,j) + 50);
+                numberCell.css("left",getLeft(i,j) + 50);
+            }
+            else{
+                numberCell.css("width","100px");
+                numberCell.css("height","100px");
+                numberCell.css("top",getTop(i));
+                numberCell.css("left",getLeft(j));
+                numberCell.css("background-color",getNumberBackgroundColor( board[i][j] ) );
+                numberCell.css("color",getNumberColor( board[i][j] ) );
+                numberCell.text( board[i][j] );
+            }
+
+	}
+}
+
+function generateOneNumber(){
+	 //To judge whether can add new number
+	 if (noSpace(board))
+	     return false;
+	 
+	 //pick a ramdom position
+	 getEmptyGrid();
+	 var rand_x = emptyGridPos[0];
+	 var rand_y = emptyGridPos[1];
+	 
+	 //generate a ramdom number
+	 var randomNum = Math.random()<0.5 ? 2:4;
+	 board[rand_x][rand_y]=randomNum;
+	 
+	 showRandomNum(rand_x,rand_y,randomNum);
+	 return true;
+}
+
+
+function girdMove(event){
+	switch(event.keyCode){
+		case 37 : //Key Left
+			if (moveLeft()){
+				generateOneNumber();
+				gameStatus();
+			}
+			break;
+			
+	   	case 38 : //Key Up
+			if (moveUp()){
+				generateOneNumber();
+				gameStatus();
+			}
+			break;
+			
+		case 39 : //Key right
+			if (moveRight()){
+				generateOneNumber();
+				gameStatus();
+			}
+			break;
+		
+		case 40 : //Key Down
+			if (moveDown()){
+				generateOneNumber();
+				gameStatus();
+			}
+			break;
+			
+		default:
+		   break;
+	}
 }
--- js/support.js
+++ js/support.js
@@ -1,8 +1,131 @@
-
-function getTop(x){
-	return 20+120*x;
+function getTop(x) {
+	return 20 + 120 * x;
 }
 
-function getLeft(y){
-	return 20+120*y;
+function getLeft(y) {
+	return 20 + 120 * y;
 }
+
+//the background color for specific digit
+function getNumberBackgroundColor(number) {
+	switch(number) {
+		case 2:
+			return "#eee4da";
+			break;
+		case 4:
+			return "#ede0c8";
+			break;
+		case 8:
+			return "#f2b179";
+			break;
+		case 16:
+			return "#f59563";
+			break;
+		case 32:
+			return "#f67c5f";
+			break;
+		case 64:
+			return "#f65e3b";
+			break;
+		case 128:
+			return "#edcf72";
+			break;
+		case 256:
+			return "#edcc61";
+			break;
+		case 512:
+			return "#9c0";
+			break;
+		case 1024:
+			return "#33b5e5";
+			break;
+		case 2048:
+			return "#09c";
+			break;
+		case 4096:
+			return "#a6c";
+			break;
+		case 8192:
+			return "#93c";
+			break;
+	}
+
+	return "black";
+}
+
+//the color attribute for specific digit
+function getNumberColor(number) {
+	if(number <= 4)
+		return "#776e65";
+
+	return "white";
+}
+
+function noSpace(board) {
+	for(var i = 0; i < 4; i++) {
+		for(var j = 0; j < 4; j++) {
+			if(board[i][j] == 0)
+				return false; //there is an empty grid
+		}
+	}
+	return true; //there is no empty grid
+}
+
+function getEmptyGrid() {
+	var emptyGrid = new Array();
+	var z = 0;
+	for(var i = 0; i < 4; i++) {
+		for(var j = 0; j < 4; j++) {
+			if(board[i][j] == 0) {
+				emptyGrid[z] = new Array();
+				emptyGrid[z][0] = i;
+				emptyGrid[z][1] = j;
+				z++;
+			}
+
+		}
+	}
+	var randomIndex = parseInt(Math.floor(Math.random() * emptyGrid.length));
+	//var randomPos = new Array();
+	emptyGridPos[0] = emptyGrid[randomIndex][0];
+	emptyGridPos[1] = emptyGrid[randomIndex][1];
+	//return randomPos;
+}
+
+function showRandomNum(x, y, num) {
+	var numberCell = $("#number-cell-" + x + "-" + y);
+
+	numberCell.css("background-color", getNumberBackgroundColor(num)); 
+	numberCell.css("color", getNumberColor(num)); 
+	numberCell.text(num);
+
+	numberCell.animate({
+		//the initial witdth/height is 0px
+		width: "100px",
+		height: "100px",
+		top:getTop(x),
+		left:getLeft(y)
+		}, 80);
+}
+
+//indeed, even only one grid which is movable can return true
+function canMoveLeft(board){
+	for(var x=0;x<4;x++){
+		for(var y = 1;y<4;y++){
+			if(board[x][y-1]==0||board[x][y-1]==board[x][y])
+			return true;
+		}
+	}
+	
+	return false;
+}
+
+function moveLeft(){
+	//To confirm whether the gird can be moved towards left
+	if(!canMoveLeft())	
+	   return false;
+	//To move left 
+	
+}
+
+
